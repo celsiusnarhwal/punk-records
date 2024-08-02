2b3f03a7734410a1274e4cfe6bd1b4dca45fd743
celsius narhwal <hello@celsiusnarhwal.dev>
2024-08-02 06:13:58 -0400

    Change base URL to https://tcbscans.me

--- labosphere/constants.py
+++ labosphere/constants.py
@@ -6,6 +6,6 @@ from yarl import URL
 DEV_MODE = os.getenv("DEV_MODE")
 LABOSPHERE_DIR = Path(__file__).parent
 ROOT = LABOSPHERE_DIR.parent
-BASE_URL = URL("https://tcb-backup.bihar-mirchi.com")
+BASE_URL = URL("https://tcbscans.me")
 GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
 DOCKER = os.getenv("DOCKER")
